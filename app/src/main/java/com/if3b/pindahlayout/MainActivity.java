a80a55299b14ac6bdce05256c987c223ba9ddbc9
DarkLeZx <depniel08@mhs.mdp.ac.id>
2022-10-03 11:02:41 +0700

    Finish Explisit
    Menyelesaikan Eksplisit

--- app/src/main/java/com/if3b/pindahlayout/MainActivity.java
+++ app/src/main/java/com/if3b/pindahlayout/MainActivity.java
@@ -2,13 +2,26 @@ package com.if3b.pindahlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnPindah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Button Pindah");
+        btnPindah = findViewById(R.id.btn_pindah);
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindahKuliah = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(pindahKuliah);
+            }
+        });
     }
 }
